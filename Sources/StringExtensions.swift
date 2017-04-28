3ab5a3270ca2dd82f2ab1a8aebd1f21ad4a7d2db
Sai628 <sai100628@gmail.com>
2017-04-28 18:48:14 +0800

    StringExtensions add “public” keyword

--- Sources/StringExtensions.swift
+++ Sources/StringExtensions.swift
@@ -16,7 +16,7 @@
 
 extension String {
     /// EZSE: Init string with a base64 encoded string
-    init ? (base64: String) {
+    public init ? (base64: String) {
         let pad = String(repeating: "=", count: base64.length % 4)
         let base64Padded = base64 + pad
         if let decodedData = Data(base64Encoded: base64Padded, options: NSData.Base64DecodingOptions(rawValue: 0)), let decodedString = NSString(data: decodedData, encoding: String.Encoding.utf8.rawValue) {
@@ -27,7 +27,7 @@ extension String {
     }
     
     /// EZSE: base64 encoded of string
-    var base64: String {
+    public var base64: String {
         let plainData = (self as NSString).data(using: String.Encoding.utf8.rawValue)
         let base64String = plainData!.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
         return base64String
